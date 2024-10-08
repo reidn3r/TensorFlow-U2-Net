4c25ad6a64234941d970c739b1a3bb3ac3af93ab
reidn3r <reidnersteam@hotmail.com>
2024-10-07 23:32:04 -0300

    model: hugging face model uploaded

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
-import tensorflow as tf
-from dotenv import load_dotenv
+from tensorflow import keras
+from huggingface_hub import hf_hub_download
 from inference.single_shot import single_shot
+from model.u2net import u2net
 
-load_dotenv() 
+os.environ["KERAS_BACKEND"] = "tensorflow"
 def main():
-    model = tf.keras.models.load_model('u2net-512.keras')
+    model = u2net(input_shape=(512,512,3))
+    
+    weights_path = hf_hub_download(repo_id="reidn3r/u2net-image-rembg", filename="model.weights.h5")    
+    model = model.load_weights(weights_path)    
     single_shot('./io/test.jpg', './io/result.png', model)
 
 
